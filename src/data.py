c247b131bfaf3f74c198b85133f4111a7891cca7
michaelgobz <raniahgobz@outlook.com>
2024-08-06 17:21:59 +0000

    using the random affine methods to do data argumentation

--- src/data.py
+++ src/data.py
@@ -46,8 +46,8 @@ def get_data_loaders(
             [
                 transforms.Resize(256),
                 transforms.RandomCrop(224),
-                transforms.RandomHorizontalFlip(0.2),
                 transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+                transforms.RandomAffine(degrees=(-30, 30), translate=(0.1, 0.1)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean, std),
             ]
